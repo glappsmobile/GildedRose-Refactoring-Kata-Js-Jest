2d807e2ab172ad685354ec67875f31ab5726345b
Glauco Villas Boas <glappsmobile@gmail.com>
2022-01-22 21:27:29 -0300

    created test for ordinary items with stats above 0

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,22 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+  it("should decrease sellIn and quality by 1 for ordinary items with sellIn and quality above 0", function() {
+    const gildedRose = new Shop([
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Mysterious Powder", 1, 1),
+      new Item("Unknown Seed", 50, 50),
+    ]);
+
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    console.log(items)
+    expect(items).toEqual([
+        { name: "+5 Dexterity Vest", sellIn: 9, quality: 19},
+        { name: "Elixir of the Mongoose", sellIn: 4, quality: 6},
+        { name: "Mysterious Powder", sellIn: 0, quality: 0},
+        { name: "Unknown Seed", sellIn: 49, quality: 49},
+    ]);
   });
+  
 });
