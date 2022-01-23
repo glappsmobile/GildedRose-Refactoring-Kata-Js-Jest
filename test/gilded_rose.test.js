1490e9e42900e37b9cf96d4cebaa2819028a2628
Glauco Villas Boas <glappsmobile@gmail.com>
2022-01-22 21:35:10 -0300

    created test for ordinary items with quality equals to 0

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,7 +1,7 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should decrease sellIn and quality by 1 for ordinary items with sellIn and quality above 0", function() {
+  it("should decrease sellIn and quality by 1 for ordinary items with sellIn and quality greater than 0", function() {
     const gildedRose = new Shop([
       new Item("+5 Dexterity Vest", 10, 20),
       new Item("Elixir of the Mongoose", 5, 7),
@@ -10,12 +10,30 @@ describe("Gilded Rose", function() {
     ]);
 
     const items = gildedRose.updateQuality();
-    console.log(items)
+
     expect(items).toEqual([
         { name: "+5 Dexterity Vest", sellIn: 9, quality: 19},
         { name: "Elixir of the Mongoose", sellIn: 4, quality: 6},
         { name: "Mysterious Powder", sellIn: 0, quality: 0},
         { name: "Unknown Seed", sellIn: 49, quality: 49},
+    ]);
+  });
+
+  it("should keep quality at 0 for ordinary items with quality equals to 0 ", function() {
+    const gildedRose = new Shop([
+      new Item("+5 Dexterity Vest", 10, 0),
+      new Item("Elixir of the Mongoose", 5, 0),
+      new Item("Mysterious Powder", 1, 0),
+      new Item("Unknown Seed", 50, 0),
+    ]);
+
+    const items = gildedRose.updateQuality();
+    
+    expect(items).toEqual([
+        { name: "+5 Dexterity Vest", sellIn: 9, quality: 0},
+        { name: "Elixir of the Mongoose", sellIn: 4, quality: 0},
+        { name: "Mysterious Powder", sellIn: 0, quality: 0},
+        { name: "Unknown Seed", sellIn: 49, quality: 0},
     ]);
   });
   
